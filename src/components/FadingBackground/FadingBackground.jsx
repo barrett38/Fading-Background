3ee5aa21dded5b7bebfaedc975f31ac4dba610aa
Barrett Anderson <barrett1614@gmail.com>
2025-03-27 11:21:45 -0500

    Adjusted JSX

--- src/components/FadingBackground/FadingBackground.jsx
+++ src/components/FadingBackground/FadingBackground.jsx
@@ -1,14 +1,27 @@
 import React, { useEffect, useState } from "react";
 import "./FadingBackground.css";
 
+// How many seconds to wait before changing color
+var seconds = 3;
+
+// Insert Colors that will transition
+const colors = [
+  "#FF5733",
+  "#33FF57",
+  "#3357FF",
+  "#FF33A1",
+  "#FFD700",
+  "#3357FF",
+];
+
 const FadingBackground = () => {
-  const colors = ["#FF5733", "#33FF57", "#3357FF", "#FF33A1", "#FFD700"];
   const [currentColorIndex, setCurrentColorIndex] = useState(0);
+  var intervalTime = seconds * 1000;
 
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentColorIndex((prevIndex) => (prevIndex + 1) % colors.length);
-    }, 3000); // Change every 3 seconds
+    }, intervalTime);
 
     return () => clearInterval(interval); // Cleanup on unmount
   }, []);
