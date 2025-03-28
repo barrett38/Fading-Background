0f34a2937233e36910137a71578c76578eabbaad
Barrett Anderson <barrett1614@gmail.com>
2025-03-28 08:57:46 -0500

    CHanging colors

--- src/components/FadingBackground/FadingBackground.jsx
+++ src/components/FadingBackground/FadingBackground.jsx
@@ -2,10 +2,22 @@ import React, { useEffect, useState } from "react";
 import "./FadingBackground.css";
 
 // FIRST: Set the time in seconds for the transition
-var seconds = 3;
+var seconds = 6;
 
 // SECOND: Insert Colors that will transition
-const colors = ["#FF5733", "#33FF57", "#3357FF", "#FF33A1", "#FFD700"];
+const colors = [
+  "#89CFF0", // Baby Blue
+  "#ADD8E6", // Light Blue
+  "#98FB98", // Pale Green
+  "#90EE90", // Light Green
+  "#E6E6FA", // Lavender
+  "#D8BFD8", // Thistle (Soft Purple)
+  "#F4C2C2", // Baby Pink
+  "#FFD1DC", // Light Pink
+  "#D2B48C", // Tan
+  "#A67B5B", // Soft Brown
+  "#F5F5DC", // Beige
+];
 
 const FadingBackground = () => {
   const intervalTime = seconds * 1000;
